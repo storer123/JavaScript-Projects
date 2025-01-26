9cdbe6fd3474e2517d6403756c0f1fb0c6bd7def
storer123 <104432079+storer123@users.noreply.github.com>
2025-01-27 01:28:16 +0530

    Update all_about_js_advanced.js

--- all_about_js_advanced.js
+++ all_about_js_advanced.js
@@ -1,3 +1,13 @@
+// user stories - application functionality from user's perspective
+//planning steps - user stories -> features -> flowchart(what we build) -> architecture(what holds code together/how we build)
+//development steps
+// user story  - who what why
+//async - an operation that takes some time, and after it completes, other operations can start
+//geolocation api is also a browser api
+//will display the map using 3rd party library - leaflet
+//If multiple script, any variable that is global can be acessed by any script that appears afterwards in html,
+//we want coordinates of map, so we cant use event listner on screen, use "on" property of leaflet, vey similar to add event listner
+
 
 //In JavaScript, destructuring is a shorthand syntax that allows you to unpack values from arrays or objects into distinct variables. 
 // nested and default destructuring
